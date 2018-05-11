10ad1f17a80bb31c0d40418ecdce0883f364107a
Eko Wibowo <eko@monetizemore.com>
2018-05-11 22:36:50 +0700

    [WHZ-2] Run with template

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,10 +1,9 @@
 from django.views.generic import TemplateView
 from .tasks import show_hello_world
 
-# Create your views here.
 
-class ShowHelloWorld(TemplateView):
-    template_name='hello_world.html'
+class Home(TemplateView):
+    template_name = 'home.html'
 
     def get(self, *args, **kwargs):
         show_hello_world.apply()
