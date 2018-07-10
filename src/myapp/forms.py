9169f487f0348a8721c5e2b0d344c0905a890e45
Eko Wibowo <eko@monetizemore.com>
2018-07-10 13:55:45 +0700

    [ige] path fix

--- src/myapp/forms.py
+++ src/myapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+# test
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254)
